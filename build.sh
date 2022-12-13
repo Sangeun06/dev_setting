d2f91912a9239ce2ff92f93e3fac0059b4edefdb
sangeun_kim <sangeun_kim@tmax.co.kr>
2022-12-13 17:44:44 +0900

    [feat] build 스크립트 수정
    - jellybeans color scheme이 없는 경우, 다운로드 받아서 적용하도록 수정
    - git completion 설정 파일 추가
    - 파일 및 디렉토리 검사 로직 수정

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# environments
+# Environments
 cat >> ~/.bashrc << EOF
 export PS1="\[$(tput setaf 1)\]\u@\h:\[$(tput setaf 7)\]\W\\$ \[$(tput sgr0)\]"
 alias gr='grep -Hirn'
@@ -9,7 +9,7 @@ export TERM="xterm-256color"
 source ~/.git-completion.bash
 EOF
 
-# packages
+# Packages
 install_deps=(
 	bash-completion
 	cmake
@@ -26,21 +26,34 @@ install_deps=(
 sudo apt update
 sudo apt install ${install_deps[*]}
 
-# install configuration files
+# Download jellybeans color scheme
+if [ ! -f "${HOME}/.vim/colors/jellybeans.vim" ]; then
+  mkdir -p ~/.vim/colors
+  curl https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim \
+    -o ~/.vim/colors/jellybeans.vim
+fi
+
+# Git command autocompletion
+if [ ! -f "${HOME}/.git-completion.bash" ]; then
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+    -o ~/.git-completion.bash
+fi
+
+# Install configuration files
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 cp gitconfig ~/.gitconfig
 sudo chmod 775 mkcscope && sudo cp mkcscope /usr/local/bin
-if [ ! -d "~/.vim/plugin" ]; then
+if [ ! -d "${HOME}/.vim/plugin" ]; then
   mkdir -p ~/.vim/plugin
 fi
 cp cscope_maps.vim ~/.vim/plugin
 
-if [ ! -d "~/.vim/bundle" ]; then
+if [ ! -d "${HOME}/.vim/bundle" ]; then
   mkdir -p ~/.vim/bundle
 fi
 
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
+if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
